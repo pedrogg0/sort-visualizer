aa4db5fc48f8aee40f0ab76b530dae049e346efe
pedrogg0 <pedrojosegarcia00@gmail.com>
2024-01-30 19:38:27 +0100

    added dropdown, implementing bubble sort

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class Element {
         this.newDiv = newDiv;
     }
 }
-
+//TODO: Merge with randomize
 function insertElements(numElements, elementsList) {
     var visualizer = document.getElementById("visualizer");
     for(var i = 0; i < numElements; i++) {
@@ -17,7 +17,7 @@ function insertElements(numElements, elementsList) {
         visualizer.appendChild(element.newDiv);
     }
 }
-
+//Show the elements of the list in the visualizer
 function showElements(elementsList){
     var visualizer = document.getElementById("visualizer");
     while (visualizer.lastElementChild) {
@@ -56,19 +56,93 @@ async function selectionSort(elementsList) {
     return elementsList;
 }
 
+async function bubbleSort(elementsList){
+    for (var i = 0; i < elementsList.length; i++) {
+ 
+        // Last i elements are already in place  
+        for (var j = 0; j < (elementsList.length - i - 1); j++) {
+ 
+            // Checking if the item at present iteration 
+            // is greater than the next iteration
+            if (elementsList[j] > elementsList[j + 1]) {
+                
+                // If the condition is true
+                // then swap them
+                var temp = elementsList[j]
+                elementsList[j] = elementsList[j + 1]
+                elementsList[j + 1] = temp
+            }
+        }
+    }
+ 
+    // Print the sorted elementsListay
+    console.log(elementsList);
+}
+
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
+//Play the currently selected algorithm
 function play(){
-    selectionSort(elementsList);
+    const currentAlgorithm = document.getElementById("current-algorithm").innerHTML;
+    switch (currentAlgorithm) {
+        case "Selection Sort":
+            selectionSort(elementsList);
+            break;
+        case "Bubble sort":
+            bubbleSort(elementsList);
+        default:
+            break;
+    }
 }
 
+//Clean and fill the list with random value elements
 function randomize(){
     elementsList = [];
     showElements(elementsList)
     insertElements(20, elementsList);
 }
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function changeAlgorithm() {
+    document.getElementById("dropdown").classList.toggle("show");
+}
+
+//Function to change the name of the current algorithm
+function changeCurrentAlgorithm(currentAlgorithm){
+    document.getElementById("current-algorithm").innerHTML = currentAlgorithm;
+    
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+var algorithmList = ["Selection Sort", "Bubble sort"] //All available algorithms 
+changeCurrentAlgorithm(algorithmList[0]); //Start displaying the first algorithm of the list
+
+dropdown = document.getElementById("dropdown");
+
+//Fill dropdown with every algorithm available
+algorithmList.forEach(element => {
+    var newBtn = document.createElement('button');
+    newBtn.innerHTML = element;
+    newBtn.onclick = function() { changeCurrentAlgorithm(element); }; //Add onClick event to change selectedAlgorithm
+    dropdown.appendChild(newBtn);
+});
+
 
 var elementsList = [];
 insertElements(20, elementsList);
